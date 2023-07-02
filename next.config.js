df734191d817f60619d6ddae027c0088cfe46554
Andre Neves <andre@zebedee.io>
2023-07-02 12:44:38 -0700

    feature: adding next.config.js rejection if env var not available and adding dotenv for checking

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+require('dotenv').config();
 
-module.exports = nextConfig
+module.exports = () => {
+    // Check if the required environment variable is present
+    if (!process.env.ZEBEDEE_API_KEY) {
+      throw new Error('ZEBEDEE_API_KEY environment variable is required. Please set a .env.local file with the proper API Keys to get started.');
+    }
+
+    // Your other Next.js configuration
+    return {
+      reactStrictMode: true,
+    }
+};
